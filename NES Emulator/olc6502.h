c14e6f37dbeb5f0899bb95befad11d20b8914d32
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2022-09-13 23:59:14 -0400

    Update 1

--- NES Emulator/olc6502.h
+++ NES Emulator/olc6502.h
@@ -1,5 +1,15 @@
 #pragma once
+class Bus;
 class olc6502
 {
+public:
+	olc6502();
+	~olc6502();
+
+	void ConnectBus(Bus* n) { bus = n; }
+private:
+	Bus* bus = nullptr;
+	uint8_t read(uint16_t a);
+	void write(uint16_t a, uint8_t d);
 };
 
