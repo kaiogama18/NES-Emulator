5b6c92bde1feee28e0fc29ff68edbd545cc52da6
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2022-09-14 23:56:28 -0400

    CPU day 2

--- NES Emulator/olc6502.cpp
+++ NES Emulator/olc6502.cpp
@@ -1,1 +1,16 @@
 #include "olc6502.h"
+#include <cstdint>
+#include "Bus.h"
+
+
+olc6502::olc6502() {
+
+}
+
+uint8_t olc6502::read(uint16_t a) {
+	return bus->read(a, false);
+}
+
+void olc6502::write(uint16_t a, uint8_t d) {
+	bus->write(a, d);
+}
